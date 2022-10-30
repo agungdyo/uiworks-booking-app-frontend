92693ebb1cdafb51ba5f57ea27213db912916b01
Putra Abbas <pabbas@makaramas.com>
2022-10-30 23:23:25 +0700

    update environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  api: 'https://tennis.uipay.id/api/v1',
+  maja_api: 'https://billing.maja.id/api/v2',
+  token_URL: 'https://account.makaramas.com/auth/realms/maja/protocol/openid-connect/token',
+  client_id: 'MAJA80114',
+  client_secret: 'TX2cTICuwCzeUT2bnMNK1IIbXRr7f8UF',
+  username: '80114',
+  password: '80114'
 };
