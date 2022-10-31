d98bd5302dbf2e7ea37068358c587490b5550162
Putra Abbas <pabbas@makaramas.com>
2022-10-31 11:55:42 +0700

    debug payment page

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -22,8 +22,8 @@ export class PaymentPage implements OnInit {
 
   onLoad(){
     this.registrationService.getReservation({
-      startDate: this.start_date.toLocaleString('en-CA').slice(0,10),
-      endDate: this.end_date.toLocaleString('en-CA').slice(0,10),
+      startDate: new Date(this.start_date).toLocaleString('en-CA').slice(0,10),
+      endDate: new Date(this.end_date).toLocaleString('en-CA').slice(0,10),
       paid: true
     },'').subscribe({
       next: (res) => {
